3132e5c2c9aa9899e6752a12d1199d1c742e9362
joao-paulo-parity <joao-paulo@parity.io>
2022-01-14 09:00:42 -0300

    move Logger to the logger module

---
+++ src/logger.ts
@@ -0,0 +1,51 @@
+import os from "os"
+import { inspect } from "util"
+
+// GitHub by default logs N lines when the job is starting.
+// N = 4 for pr-custom-review:
+// 1. Run org/pr-custom-review@tag
+// 2.   with:
+// 3.     token: ***
+// 4.     config-file: ./.github/pr-custom-review-config.yml
+const githubLogsInitialLineCount = 4
+
+export class Logger {
+  lineCount: number = 0
+  public relevantStartingLine: number = 1
+
+  constructor() {}
+
+  doLog = (...args: any[]) => {
+    const message = args
+      .map(function (arg) {
+        return typeof arg === "string" ? arg : inspect(arg)
+      })
+      .join(" ")
+    process.stdout.write(`${message}${os.EOL}`)
+    this.lineCount += (message.match(/\n/g) || "").length + 1
+  }
+
+  public log = (...args: any[]) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    this.doLog(...args)
+  }
+
+  public error = (...args: any[]) => {
+    // Uses escape codes for displaying the error line as red.
+    // https://dustinpfister.github.io/2019/09/19/nodejs-ansi-escape-codes/
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    this.doLog("\n\u001b[31;1;4mERROR: ", ...args, "\u001b[39m")
+  }
+
+  public warning = (...args: any[]) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    this.doLog("WARNING: ", ...args)
+  }
+
+  public markNextLineAsRelevantStartingLine = (delta = 1) => {
+    this.relevantStartingLine =
+      this.lineCount + githubLogsInitialLineCount + delta
+  }
+}
+
+export default Logger
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import * as core from "@actions/core"
 import * as github from "@actions/github"
-import { Context } from "@actions/github/lib/context"
 import * as Webhooks from "@octokit/webhooks-types"
 import * as fs from "fs"
 import Joi from "joi"
-import { inspect } from "util"
 import * as YAML from "yaml"
-import * as os from "os"
+
+import Logger from "./logger"
 
 type Octokit = ReturnType<typeof github.getOctokit>
 type PR =
@@ -37,52 +36,6 @@ const configurationSchema = Joi.object<Configuration>().keys({
 })
 
 type RuleUserInfo = { team: string | null }
-
-// GitHub by default logs N lines when the job is starting.
-// N = 4 for pr-custom-review:
-// 1. Run org/pr-custom-review@tag
-// 2.   with:
-// 3.     token: ***
-// 4.     config-file: ./.github/pr-custom-review-config.yml
-const githubLogsInitialLineCount = 4
-class Logger {
-  lineCount: number = 0
-  public relevantStartingLine: number = 1
-
-  constructor() {}
-
-  doLog = (...args: any[]) => {
-    const message = args
-      .map(function (arg) {
-        return typeof arg === "string" ? arg : inspect(arg)
-      })
-      .join(" ")
-    process.stdout.write(`${message}${os.EOL}`)
-    this.lineCount += (message.match(/\n/g) || "").length + 1
-  }
-
-  public log = (...args: any[]) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    this.doLog(...args)
-  }
-
-  public error = (...args: any[]) => {
-    // Uses escape codes for displaying the error line as red.
-    // https://dustinpfister.github.io/2019/09/19/nodejs-ansi-escape-codes/
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    this.doLog("\n\u001b[31;1;4mERROR: ", ...args, "\u001b[39m")
-  }
-
-  public warning = (...args: any[]) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    this.doLog("WARNING: ", ...args)
-  }
-
-  public markNextLineAsRelevantStartingLine = (delta = 1) => {
-    this.relevantStartingLine =
-      this.lineCount + githubLogsInitialLineCount + delta
-  }
-}
 
 const combineUsers = async function (
   pr: PR,
