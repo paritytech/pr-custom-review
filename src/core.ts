9689d9e3ef8b6f85a93d0563a399d362ce8870de
João Paulo Silva de Souza <77391175+joao-paulo-parity@users.noreply.github.com>
2022-02-09 10:37:34 -0300

    Fixes for private repositories (#53)
    * Add authenticated request for diff body
    
    * fix formatting
    
    * use more type-safe octokit API
    
    Co-authored-by: Sergejs Kostjucenko <sergejs@parity.io>

--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,4 @@
+import { OctokitResponse } from "@octokit/types"
 import YAML from "yaml"
 
 import {
@@ -100,9 +101,12 @@ export const runChecks = async function (
     return commitStateFailure
   }
 
-  const diffResponse: { data: string; status: number } = await octokit.request(
-    pr.diff_url,
-  )
+  const diffResponse = (await octokit.rest.pulls.get({
+    owner: pr.base.repo.owner.login,
+    repo: pr.base.repo.name,
+    pull_number: pr.number,
+    mediaType: { format: "diff" },
+  })) /* Octokit doesn't inform the right return type for mediaType: { format: "diff" } */ as unknown as OctokitResponse<string>
   if (diffResponse.status !== 200) {
     logger.failure(
       `Failed to get the diff from ${pr.diff_url} (code ${diffResponse.status})`,
