a08cbb070620061db5b3f554db589a2c1c59ae0a
joao-paulo-parity <joao-paulo@parity.io>
2022-04-19 10:49:22 -0300

    fix example env

--- .env.example.cjs
+++ .env.example.cjs
@@ -2,14 +2,17 @@
 
 // All environment variables are required unless explicitly told otherwise
 
-// Set LOG_FORMAT to "json" for JSON logging or leave it as "none"
-process.env.LOG_FORMAT ??= "none"
-
 // Defines a port for the HTTP server
 process.env.PORT ??= 3000
 
 /*
-  Guidance for registering an app is at:
+  Guidance for registering a token is provided at
   https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
 */
 process.env.GITHUB_ACCESS_TOKEN ??= "placeholder"
+
+/*
+  NOT REQUIRED
+  Set LOG_FORMAT to "json" for JSON logging
+*/
+// process.env.LOG_FORMAT ??= "json"
